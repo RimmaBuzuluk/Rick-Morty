5eddda0a04ef569dca548b12c662a559325cf792
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-03-13 11:48:49 +0200

    input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Block_info from "./component/Block_info";
 import Img from "./component/Img";
 
 function App() {
   return (
     <div className="App">
       <Img />
+      <Block_info />
     </div>
   );
 }
