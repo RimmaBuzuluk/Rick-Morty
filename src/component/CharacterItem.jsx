4aab11baff34ad3fb036357f5a48906eb7ec6144
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-03-20 15:33:24 +0200

    rout

--- src/component/CharacterItem.jsx
+++ src/component/CharacterItem.jsx
@@ -3,17 +3,25 @@ import { observer } from "mobx-react-lite";
 import { Context } from "../index";
 import { Card, Col, Image, Row } from "react-bootstrap";
 import "../App.css";
+import { PAGE } from "../utils/consts";
+import { useNavigate } from "react-router-dom";
 
 const CharacterItem = ({ character }) => {
+  const navigate = useNavigate();
   return (
-    <Col md={3} className={"mt-3 "}>
+    <Col
+      md={3}
+      className={"mt-3 "}
+      onClick={() => navigate(PAGE + "/" + character.id)}
+    >
       <Card
-        className="mx-auto d-block"
-        style={{ width: 150, cursor: "pointer" }}
+        className="mx-auto d-block bshadow"
+        style={{ width: 200, cursor: "pointer" }}
         border={"light"}
       >
-        <Image width={150} height={150} src={character.img} />
+        <Image width={200} height={150} src={character.img} />
         <div>{character.name}</div>
+        <div>{character.person}</div>
       </Card>
     </Col>
   );
