32c4d6f1e0d7788d93ccaca1eecc7242f60157a7
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-06-08 16:20:37 +0300

    startsWith

--- src/component/Serch.jsx
+++ src/component/Serch.jsx
@@ -4,36 +4,34 @@ import { Context } from "../index";
 function Search() {
   const { character } = useContext(Context);
 
-  const [searchTerm, setSearchTerm] = useState('');
-  const [matchingCharacters, setMatchingCharacters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');//serchTerm зберігає значення поточного запросу
+  // const [matchingCharacters, setMatchingCharacters] = useState([]);
 
+  //сортування за алфавітом
   const sortData=[...character.character].sort((a,b)=>{
     return a.name>b.name ? 1:-1
   })
-
-  console.log(sortData)
-
-  
-//   const handleSearchChange = (event) => {
-
-//     const search = event.target.value.toLowerCase();//отримуемо [елементи
-//     // const matching = character.character.filter((character) => character.toLowerCase().includes(search));
-//     const NewDataSerch=[]
-//     for(let i=0;i<sortData.length;i++){
-//       const matching=sortData[i].name
-//       NewDataSerch.push(matching)
-//       // console.log(matching)
-//     }
-//     console.log(NewDataSerch)
-// //     const matching=sortData[1].name
-// // console.log(matching)
-//     // setMatchingCharacters(matching.sort());
-//     // setSearchTerm(search);
-//   };
+  // console.log(sortData)
 
 
-  // console.log(characters)
-  return <input type="text"  placeholder="Filter by name..." />;
+//сортування за пошуком
+
+const handleInputChange = (event) => {
+  setSearchTerm(event.target.value);
+};
+
+// console.log(handleInputChange)
+// console.log(character)
+
+const filteredData=sortData.filter(item => item.name.startsWith(searchTerm))
+console.log(filteredData)
+
+
+  return <input
+  type="text"
+  placeholder="Filter by name..."
+  onChange={handleInputChange}
+/>;
 }
 
 export default Search;
