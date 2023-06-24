75d69cda9599cd3ba2a491ee8996d731e7455d10
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-06-24 21:36:05 +0300

    ImgBlock

--- src/component/ImgBlock.jsx
+++ src/component/ImgBlock.jsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
 import { useParams } from "react-router-dom";
 import CharacterStore from "../store/CharacterStore";
+import { Image } from "react-bootstrap";
 
 const ImgBlock = observer(() => {
   const { id } = useParams();
@@ -9,11 +10,45 @@ const ImgBlock = observer(() => {
   console.log(selectedCharacter)
 
   return (
-    <div>
- <p>hello</p>
- <h1>{selectedCharacter.name}</h1>
-    </div>
-  );
+//     <div>
+//  <p>hello</p>
+//  <h1>{selectedCharacter.name}</h1>
+//     </div>
+<div>
+<Image className="mainPic" src={selectedCharacter.img} />
+<div className="nameSanchez">{selectedCharacter.name}</div>
+<div className="information">information</div>
+<div className="block">
+  <div className="block_inf">
+    <div className="MainN">Gender</div>
+    <div className="ingChar">{selectedCharacter.gender}</div>
+    <hr />
+  </div>
+  <div className="block_inf">
+    <div className="MainN">Status</div>
+    <div className="ingChar">{selectedCharacter.status}</div>
+    <hr />
+  </div>
+  <div className="block_inf">
+    <div className="MainN">Specie</div>
+    <div className="ingChar" >{selectedCharacter.specie}</div>
+    <hr />
+  </div>
+  <div className="block_inf">
+    <div className="MainN">Origin</div>
+    <div className="ingChar">{selectedCharacter.origin}</div>
+    <hr />
+  </div>
+  <div className="block_inf">
+    <div className="MainN">Type</div>
+    <div className="ingChar">{selectedCharacter.type}</div>
+    <hr />
+  </div>
+</div>
+<p>hello</p>
+<h1>{selectedCharacter.name}</h1>
+</div>
+);
 });
 
 export default ImgBlock;
